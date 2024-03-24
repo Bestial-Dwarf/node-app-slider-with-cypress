a76be6ae7547a0957e480ddd12caad574b6597ee
Bestial-Dwarf <dawidt007@wp.pl>
2024-03-24 18:31:36 +0100

    lab 1
    Zadanie 1

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,22 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+//Zweryfikuj, czy opis każdego slajdu jest wyświetlany poprawnie.
+    //Krok 1: Otwórz stronę z galerią.
+        //Krok 2: Dla każdego slajdu, sprawdź, czy tytuł i opis są wyświetlane.
+        //Krok 3: Upewnij się, że tytuł i opis odpowiadają oczekiwanym treściom.
+
+        describe('Swiper Gallery Test', function () {
+          it('Checks if second slide contains "Description"', function () {
+              cy.visit('http://localhost:3000');
+              cy.get('.swiper-slide-active').should('contain', 'Italy');
+              cy.get('.swiper-button-next').click({ force: true });
+              cy.wait(2000);
+              cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+              cy.get('.swiper-button-next').click({ force: true });
+              cy.wait(2000);
+              cy.get('.swiper-slide-active').should('contain', 'France');
+          });
+      });
+      
